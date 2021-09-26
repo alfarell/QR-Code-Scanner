7afccd27121e75d77d69425003ed667f8231ec9f
Alfarell Muchamad Yuwanto <farel@genflix.co.id>
2021-09-26 10:44:27 +0700

    Initialize App

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  bracketSpacing: true,
+  bracketSameLine: false,
+  jsxBracketSameLine: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'avoid',
+  semi: false
+};
